220a3bd18b24dc9619d4ae7548b9c952c341e16f
Basheer Shahrour <shahrourbasheer1997@gmail.com>
2021-11-27 17:23:13 +0200

    make the db connection in separate file

--- database/connection.go
+++ database/connection.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/basheer-shahrour/gin-server/entity"
@@ -9,30 +10,28 @@ import (
 	"gorm.io/gorm"
 )
 
-type UserRepository interface {
-	ConnectToDB() *database
+type Database interface {
+	ConnectToDB() *gorm.DB
 	CloseDB()
 }
 
-type database struct {
-	connection *gorm.DB
-}
-
-func (db *database) ConnectToDB() {
+func ConnectToDB() *gorm.DB {
 	godotenv.Load()
 	dsn := os.Getenv("DB_CONNECTION_STRING")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("connectedToDB\n")
 	db.AutoMigrate(&entity.User{}, &entity.Conversation{})
-	return &database{connection: db}
+	return db
 }
 
-func (db *database) CloseDB() {
-	sqlDB, _ := db.connection.DB()
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
 	err := sqlDB.Close()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("DB CLOSED\n")
 }
--- repository/user-repository.go
+++ repository/user-repository.go
@@ -1,11 +1,8 @@
 package repository
 
 import (
-	"os"
-
+	"github.com/basheer-shahrour/gin-server/database"
 	"github.com/basheer-shahrour/gin-server/entity"
-	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -17,43 +14,31 @@ type UserRepository interface {
 	CloseDB()
 }
 
-type database struct {
+type databaseConnection struct {
 	connection *gorm.DB
 }
 
-func NewUserRepository() *database {
-	godotenv.Load()
-	dsn := os.Getenv("DB_CONNECTION_STRING")
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-	db.AutoMigrate(&entity.User{}, &entity.Conversation{})
-	return &database{connection: db}
-
+func NewUserRepository() *databaseConnection {
+	return &databaseConnection{connection: database.ConnectToDB()}
 }
 
-func (db *database) CloseDB() {
-	sqlDB, _ := db.connection.DB()
-	err := sqlDB.Close()
-	if err != nil {
-		panic(err)
-	}
+func (db *databaseConnection) CloseDB() {
+	database.CloseDB(db.connection)
 }
 
-func (db *database) Save(user entity.User) {
+func (db *databaseConnection) Save(user entity.User) {
 	db.connection.Create(&user)
 }
 
-func (db *database) Update(user entity.User) {
+func (db *databaseConnection) Update(user entity.User) {
 	db.connection.Save(&user)
 }
 
-func (db *database) Delete(user entity.User) {
+func (db *databaseConnection) Delete(user entity.User) {
 	db.connection.Delete(&user)
 }
 
-func (db *database) FindAll() []entity.User {
+func (db *databaseConnection) FindAll() []entity.User {
 	var users []entity.User
 	db.connection.Set("gorm:auto_preload", true).Find(&users)
 	return users
